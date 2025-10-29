5248bca88cd651ae9c05537de1fc2f0be09ba0d3
nabilaboshady <nabilaboshady763@gmail.com>
2025-10-29 18:11:28 -0400

    task1

---
+++ task1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter the frist number :");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter the second number :");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine(a + b);
+    }
+  
+}
